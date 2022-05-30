5e666a84c2f1011ec3837fcbeb686d80cdb76340
Arqam Siddiqui <akkosidd@gmail.com>
2022-05-30 15:53:12 +0500

    Featured Prod Slider

--- src/app/components/featured-products/featured-products.component.ts
+++ src/app/components/featured-products/featured-products.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation , OnInit } from '@angular/core';
+import SwiperCore, { Keyboard, Pagination, Navigation, Virtual, SwiperOptions } from 'swiper';
+import { BehaviorSubject } from 'rxjs';
+
+
+SwiperCore.use([Keyboard, Pagination, Navigation, Virtual]);
+
 
 @Component({
   selector: 'app-featured-products',
@@ -6,6 +12,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./featured-products.component.css']
 })
 export class FeaturedProductsComponent implements OnInit {
+
+  config: SwiperOptions = {
+    autoHeight: true,
+    allowTouchMove: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: true
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 1,
+      },
+      500: {
+        slidesPerView: 1,
+      },
+      400: {
+        slidesPerView: 1,
+      },
+      300: {
+        slidesPerView: 1,
+      }
+    },
+    navigation: true,
+    pagination: {
+      clickable: true
+    },
+    loop: true
+  };
 
   constructor() { }
 
